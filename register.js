d9b7687b1df634fcc98214103ca4197e6697d8fa
ott <otttihvan@gmail.com>
2024-09-19 13:42:13 +0300

    test

--- register.js
+++ register.js
@@ -3,18 +3,31 @@
 const apiURL = 'https://demo2.z-bit.ee/users';
 
 
-function GetUserData(DATA) {
+
+
+
+function GetUserData() {
+
+    const data = {
+        newPassword : document.getElementById("PASSWORD").value,
+        username : document.getElementById("MAIL").value,
+        firstname : document.getElementById("FIRSTNAME").value,
+        lastname : document.getElementById("LASTNAME").value,
+    }
+
+
+    console.log(data)
     fetch(apiURL, {
         method: "POST",
-        body: JSON.stringify(DATA),
+        body: JSON.stringify(data),
         headers: {
           "Content-type": "application/json; charset=UTF-8"
         }
       })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+
 }
 
  
-
-GetUserData()
